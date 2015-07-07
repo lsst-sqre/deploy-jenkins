c24701889f2bf9b3e37bd606a48b3dae749c3bdd
Joshua Hoblitt <josh@hoblitt.com>
2015-07-07 11:39:26 -0700

    fix aws http/https security group port number
    The SSL/TLS port was typo'd as 433 instead of 443.

--- terraform/main.tf
+++ terraform/main.tf
@@ -115,8 +115,8 @@ resource "aws_security_group" "jenkins-demo-http" {
     }
 
     ingress {
-        from_port = 433
-        to_port = 433
+        from_port = 443
+        to_port = 443
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
