fb6a9c663d642f9ab101c8a17e7e607b7c73aa02
Joshua Hoblitt <josh@hoblitt.com>
2015-08-28 12:30:45 -0700

    use devtoolset-3 for CI builds on EL6

--- jenkins_demo/manifests/role/slave.pp
+++ jenkins_demo/manifests/role/slave.pp
@@ -2,4 +2,17 @@ class jenkins_demo::role::slave {
   include ::jenkins_demo::profile::base
   include ::jenkins_demo::profile::ganglia::gmond
   include ::jenkins_demo::profile::slave
+
+  if $::operatingsystemmajrelease == '6' {
+    include ::jenkins_demo::profile::devtoolset_3
+
+    Class['::jenkins_demo::profile::devtoolset_3'] ->
+      Class['::jenkins_demo::profile::slave']
+
+    file_line { 'enable devtoolset-3':
+      line    => '. /opt/rh/devtoolset-3/enable',
+      path    => '/home/build0/.bashrc',
+      require => User['build0'],
+    }
+  }
 }
