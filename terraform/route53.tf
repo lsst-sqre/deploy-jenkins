c95a6a22faabe1b9f3d578a44dd1d9500af0a162
Joshua Hoblitt <josh@hoblitt.com>
2016-06-06 08:34:22 -0700

    change default jenkins master hostname
    From `<prefix>.<domain>` -> `<prefix>-ci.<doamin>`

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "jenkins-demo-master" {
   zone_id = "${var.aws_zone_id}"
-  name = "${var.demo_name}.${var.domain_name}"
+  name = "${var.demo_name}-ci.${var.domain_name}"
   type = "A"
   ttl = "300"
   records = ["${aws_eip.jenkins-demo-master.public_ip}"]
