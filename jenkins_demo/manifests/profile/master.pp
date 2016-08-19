563c2f94f9976de5211ad260b4ec4a9d4814524a
Joshua Hoblitt <josh@hoblitt.com>
2016-08-19 10:36:46 -0700

    add jenkins node osx-1 configuration

--- jenkins_demo/manifests/profile/master.pp
+++ jenkins_demo/manifests/profile/master.pp
@@ -107,6 +107,24 @@ class jenkins_demo::profile::master {
     }
   }
 
+  $osx = hiera('jenkinsx::osx', undef)
+  if $osx {
+    file { '/var/lib/jenkins/.ssh':
+      ensure => directory,
+      owner  => 'jenkins',
+      group  => 'jenkins',
+      mode   => '0700',
+    }
+
+    file { '/var/lib/jenkins/.ssh/id_rsa':
+      ensure  => file,
+      owner   => 'jenkins',
+      group   => 'jenkins',
+      mode    => '0600',
+      content => $osx['ssh_private_key'],
+    }
+  }
+
   # XXX this is [also] a dirty hack
   $jenkins_url = hiera('jenkins_fqdn', $::jenkins_fqdn)
   file { '/var/lib/jenkins/jenkins.model.JenkinsLocationConfiguration.xml':
