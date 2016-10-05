75a6b361fc47c3aef6f8a2c8200b7e71e1e0e1b1
Joshua Hoblitt <josh@hoblitt.com>
2016-10-04 18:00:38 -0700

    bulk jenkins plugin update
    Primarily to get pickup minor improvements in pipeline/workflow but also
    motivated by trying to not drift to far behind current releases.

--- jenkins_demo/manifests/profile/master.pp
+++ jenkins_demo/manifests/profile/master.pp
@@ -172,7 +172,7 @@ class jenkins_demo::profile::master {
   $github_xml = 'github-plugin-configuration.xml'
   jenkins::plugin { 'github':
     manage_config   => true,
-    version         => '1.19.2',
+    version         => '1.22.1',
     config_filename => $github_xml,
     config_content  => template("${module_name}/plugins/${github_xml}"),
   }
@@ -180,7 +180,7 @@ class jenkins_demo::profile::master {
   $ghprb_xml = 'org.jenkinsci.plugins.ghprb.GhprbTrigger.xml'
   jenkins::plugin { 'ghprb':
     manage_config   => true,
-    version         => '1.32.8',
+    version         => '1.33.1',
     config_filename => $ghprb_xml,
     config_content  => template("${module_name}/plugins/${ghprb_xml}"),
   }
