a825f6fc363a85da128bba72cdcb30d1bb4e22c9
Joshua Hoblitt <josh@hoblitt.com>
2016-11-08 15:38:05 -0700

    add docker to slave profile

--- jenkins_demo/manifests/profile/slave.pp
+++ jenkins_demo/manifests/profile/slave.pp
@@ -1,14 +1,41 @@
 class jenkins_demo::profile::slave {
   include ::lsststack
 
+  if $::operatingsystemmajrelease == '7' {
+    include ::docker
+
+    $docker = 'docker'
+    $dockergc = '/usr/local/bin/docker-gc'
+
+    archive { 'docker-gc':
+      source  => 'https://raw.githubusercontent.com/spotify/docker-gc/master/docker-gc',
+      path    => $dockergc,
+      cleanup => false,
+      extract => false,
+    } ->
+    # this isn't nessicary with puppet/archive 1.x
+    file { $dockergc:
+      mode => '0555',
+    }
+
+    cron { 'docker-gc':
+      command => $dockergc,
+      minute  => '0',
+      hour    => '4',
+    }
+  } else {
+    $docker = undef
+  }
+
   $platform = downcase("${::operatingsystem}-${::operatingsystemmajrelease}")
   class { 'jenkins::slave':
-    masterurl  => 'http://jenkins-master:8080',
-    slave_name => $::hostname,
-    executors  => 1,
-    labels     => "${::hostname} ${platform}",
+    masterurl    => 'http://jenkins-master:8080',
+    slave_name   => $::hostname,
+    slave_groups => $docker,
+    executors    => 1,
+    labels       => "${::hostname} ${platform} ${docker}",
     # don't start slave before lsstsw build env is ready
-    require    => [
+    require      => [
       Class['lsststack'],
       Host['jenkins-master'],
     ],
