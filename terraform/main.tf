2d6788af94cae1eeff94d8d7ff250717c2dc715a
Joshua Hoblitt <josh@hoblitt.com>
2017-01-04 11:52:52 -0700

    restrict ingress ssh security group to 140.252/16
    To remove the ssh probing spam from the system logs.

--- terraform/main.tf
+++ terraform/main.tf
@@ -100,7 +100,7 @@ resource "aws_security_group" "jenkins-demo-ssh" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["140.252.0.0/16"]
   }
 
   tags {
