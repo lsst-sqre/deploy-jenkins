0b612906606f1fe52dd62042645200885f351961
Joshua Hoblitt <josh@hoblitt.com>
2017-05-03 15:12:25 -0700

    add jenkins_demo::profile::devtoolset_6

---
+++ jenkins_demo/manifests/profile/devtoolset_6.pp
@@ -0,0 +1,13 @@
+class jenkins_demo::profile::devtoolset_6 {
+
+  ensure_packages(['centos-release-scl'])
+
+  package {[
+    'devtoolset-6-gcc',
+    'devtoolset-6-gcc-c++',
+    'devtoolset-6-gcc-gfortran',
+  ]:
+    ensure  => present,
+    require => Package['centos-release-scl'],
+  }
+}
