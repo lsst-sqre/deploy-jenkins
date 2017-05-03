ad13d4f327a980082d2d6eb64c1f602e007dac3e
Joshua Hoblitt <josh@hoblitt.com>
2017-05-03 15:12:57 -0700

    add devtoolset-6 to jenkins::role::slave

--- jenkins_demo/manifests/role/slave.pp
+++ jenkins_demo/manifests/role/slave.pp
@@ -5,4 +5,5 @@ class jenkins_demo::role::slave {
   class { 'selinux': mode => 'disabled' }
   include ::jenkins_demo::profile::devtoolset_3
   include ::jenkins_demo::profile::devtoolset_4
+  include ::jenkins_demo::profile::devtoolset_6
 }
