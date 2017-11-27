7664fd6e1f25fa060503a0708de10e8e65ad19bd
Joshua Hoblitt <josh@hoblitt.com>
2017-11-27 12:02:36 -0700

    rm devtoolset-[46] from slave profile
    Unused.

--- jenkins_demo/manifests/role/slave.pp
+++ jenkins_demo/manifests/role/slave.pp
@@ -4,6 +4,4 @@ class jenkins_demo::role::slave {
   include ::jenkins_demo::profile::slave
   class { 'selinux': mode => 'disabled' }
   include ::jenkins_demo::profile::devtoolset_3
-  include ::jenkins_demo::profile::devtoolset_4
-  include ::jenkins_demo::profile::devtoolset_6
 }
