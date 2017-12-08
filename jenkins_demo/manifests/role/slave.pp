f2e98e0d09f6a607c400abb844bc11ebf869e951
Joshua Hoblitt <josh@hoblitt.com>
2017-12-07 17:45:46 -0700

    remove devtoolset-3 from nodes

--- jenkins_demo/manifests/role/slave.pp
+++ jenkins_demo/manifests/role/slave.pp
@@ -3,6 +3,5 @@ class jenkins_demo::role::slave {
   include ::jenkins_demo::profile::ganglia::gmond
   include ::jenkins_demo::profile::slave
   class { 'selinux': mode => 'disabled' }
-  include ::jenkins_demo::profile::devtoolset_3
   include ::jenkins_demo::profile::kernel
 }
