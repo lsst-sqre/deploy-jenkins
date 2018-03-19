9297dd7bd00fd37a7bce61d8121a1d2a76ed01d0
Joshua Hoblitt <josh@hoblitt.com>
2018-03-19 13:26:40 -0700

    add j_d::profile::jenkins::agent to j_d::role::master
    This is both DRYing and allows docker to be used on `jenkins-master`.

--- jenkins_demo/manifests/role/master.pp
+++ jenkins_demo/manifests/role/master.pp
@@ -3,6 +3,11 @@ class jenkins_demo::role::master {
   include ::jenkins_demo::profile::ganglia::gmond
   include ::jenkins_demo::profile::ganglia::web
   include ::jenkins_demo::profile::jenkins::master
+  # run a jnlp slave to execute jobs that need to be bound to the
+  # jenkins-master node (E.g., backups).  This provides some priviledge
+  # separation between the master process and the builds as they will be
+  # executed under the jenkins-slave user instead of the jenkins user.
+  include ::jenkins_demo::profile::jenkins::agent
   class { 'selinux': mode => 'enforcing' }
   include ::jenkins_demo::profile::kernel
 }
