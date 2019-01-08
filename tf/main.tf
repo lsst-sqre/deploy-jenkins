c699a9ee68e45852c52ccb6fe17615d1c783c43c
Joshua Hoblitt <josh@hoblitt.com>
2019-01-08 13:54:18 -0700

    declare aws provider version constraint

--- tf/main.tf
+++ tf/main.tf
@@ -3,6 +3,8 @@ terraform {
 }
 
 provider "aws" {
+  version = "~> 1.54"
+
   region     = "${var.aws_default_region}"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
