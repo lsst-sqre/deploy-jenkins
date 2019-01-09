cb923cc084de213a1945647de254a715bcffe6bb
Joshua Hoblitt <josh@hoblitt.com>
2019-01-09 09:38:39 -0700

    rm lsst specific var default

--- tf/variables.tf
+++ tf/variables.tf
@@ -5,7 +5,6 @@ variable "aws_default_region" {
 
 variable "aws_zone_id" {
   description = "route53 Hosted Zone ID to manage DNS records in."
-  default     = "Z3TH0HRSNU67AM"
 }
 
 # note that the production name is `jenkins-prod` for backwards compatiblity
@@ -20,7 +19,6 @@ variable "service_name" {
 
 variable "domain_name" {
   description = "DNS domain name to use when creating route53 records."
-  default     = "lsst.codes"
 }
 
 variable "scipipe_publish_region" {
