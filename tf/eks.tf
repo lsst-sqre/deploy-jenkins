a509ccbd44e3d9f520f3744e9863791dfdce5f71
Joshua Hoblitt <josh@hoblitt.com>
2019-04-30 15:58:42 -0700

    mv metrics-server chart deploy -> metrics-server.tf

--- tf/eks.tf
+++ tf/eks.tf
@@ -166,26 +166,3 @@ module "tiller" {
   namespace       = "${kubernetes_namespace.tiller.metadata.0.name}"
   service_account = "tiller"
 }
-
-resource "helm_release" "metrics_server" {
-  name      = "metrics-server"
-  chart     = "stable/metrics-server"
-  namespace = "kube-system"
-  version   = "2.6.0"
-
-  force_update  = true
-  recreate_pods = true
-
-  values = [
-    "${data.template_file.metrics_server_values.rendered}",
-  ]
-
-  depends_on = [
-    "null_resource.eks_ready",
-    "module.tiller",
-  ]
-}
-
-data "template_file" "metrics_server_values" {
-  template = "${file("${path.module}/charts/metrics-server.yaml")}"
-}
