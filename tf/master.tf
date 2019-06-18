4073a650a88cd7c7b51aa2aa7ff54fb1ac41e4db
Joshua Hoblitt <josh@hoblitt.com>
2019-06-18 15:13:19 -0400

    install jenkins master via helm chart

---
+++ tf/master.tf
@@ -0,0 +1,53 @@
+resource "helm_release" "jenkins" {
+  name      = "jenkins"
+  chart     = "stable/jenkins"
+  namespace = "${kubernetes_namespace.jenkins.metadata.0.name}"
+  version   = "1.3.1"
+
+  force_update  = true
+  recreate_pods = true
+
+  values = [
+    "${data.template_file.jenkins_values.rendered}",
+  ]
+
+  depends_on = [
+    "null_resource.eks_ready",
+    "module.tiller",
+  ]
+}
+
+data "template_file" "jenkins_values" {
+  template = "${file("${path.module}/charts/jenkins.yaml")}"
+
+  vars {
+    jenkins_fqdn        = "${local.master_fqdn}"
+    jenkins_secret_name = "${kubernetes_secret.jenkins_tls.metadata.0.name}"
+  }
+}
+
+resource "kubernetes_secret" "jenkins_tls" {
+  metadata {
+    name      = "jenkins-tls"
+    namespace = "${kubernetes_namespace.jenkins.metadata.0.name}"
+  }
+
+  data {
+    tls.crt = "${local.tls_crt}"
+    tls.key = "${local.tls_key}"
+  }
+
+  depends_on = [
+    "null_resource.eks_ready",
+  ]
+}
+
+resource "aws_route53_record" "jenkins" {
+  count   = "${var.dns_enable ? 1 : 0}"
+  zone_id = "${var.aws_zone_id}"
+
+  name    = "${local.master_fqdn}"
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["${local.nginx_ingress_hostname}"]
+}
