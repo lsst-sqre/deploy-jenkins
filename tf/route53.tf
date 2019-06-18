25f4cfdc1e942086f9978ce09123ddb521cfc601
Joshua Hoblitt <josh@hoblitt.com>
2019-06-18 15:13:19 -0400

    (WIP) install jenkins master via helm chart

--- tf/route53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_record" "jenkins-demo-master" {
-  zone_id = "${var.aws_zone_id}"
-
-  name    = "${local.master_fqdn}"
-  type    = "A"
-  ttl     = "60"
-  records = ["${aws_eip.jenkins-demo-master.public_ip}"]
-}
