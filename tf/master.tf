e2c2a6c960014b35f766041d3c53ebc214661826
Joshua Hoblitt <josh@hoblitt.com>
2019-06-21 10:58:18 -0400

    add VAULT_CASC_* env var for jenkins casc plugin

--- tf/master.tf
+++ tf/master.tf
@@ -21,8 +21,10 @@ data "template_file" "jenkins_values" {
   template = "${file("${path.module}/charts/jenkins.yaml")}"
 
   vars {
-    jenkins_fqdn        = "${local.master_fqdn}"
-    jenkins_secret_name = "${kubernetes_secret.jenkins_tls.metadata.0.name}"
+    jenkins_fqdn           = "${local.master_fqdn}"
+    jenkins_secret_name    = "${kubernetes_secret.jenkins_tls.metadata.0.name}"
+    casc_vault_secret_name = "${kubernetes_secret.casc_vault.metadata.0.name}"
+    vault_root             = "${local.vault_root}"
   }
 }
 
@@ -40,6 +42,17 @@ resource "kubernetes_secret" "jenkins_tls" {
   depends_on = [
     "null_resource.eks_ready",
   ]
+}
+
+resource "kubernetes_secret" "casc_vault" {
+  metadata {
+    name      = "casc-vault"
+    namespace = "${kubernetes_namespace.jenkins.metadata.0.name}"
+  }
+
+  data {
+    CASC_VAULT_TOKEN = "${local.casc_vault_token}"
+  }
 }
 
 resource "aws_route53_record" "jenkins" {
