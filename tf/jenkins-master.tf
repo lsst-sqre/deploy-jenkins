82cc188921a77d8344476206af0fd80b62ef7bc6
Joshua Hoblitt <josh@hoblitt.com>
2019-08-23 16:14:08 -0700

    convert env_name fact -> tf template var

--- tf/jenkins-master.tf
+++ tf/jenkins-master.tf
@@ -33,6 +33,7 @@ data "template_file" "jenkins_values" {
   vars {
     admin_email            = "nobody@${var.domain_name}"
     casc_vault_secret_name = "${kubernetes_secret.casc_vault.metadata.0.name}"
+    env_name               = "${var.env_name}"
     jenkins_fqdn           = "${local.master_fqdn}"
     jenkins_secret_name    = "${kubernetes_secret.jenkins_tls.metadata.0.name}"
     vault_root             = "${local.vault_root}"
