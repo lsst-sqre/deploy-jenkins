80245a9b5cccc593d169142437131a7fea51dc3d
Joshua Hoblitt <josh@hoblitt.com>
2019-09-23 20:48:38 -0700

    make shellcheck happy

--- run-casc.sh
+++ run-casc.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-FACTER_pwd=$(pwd) FACTER_group_name=dm FACTER_env_name=prod bundle exec puppet apply --modulepath=$(pwd)/modules --hiera_config=./hiera.yaml ./casc.pp
+export FACTER_pwd
+FACTER_pwd=$(pwd)
+export FACTER_group_name=dm
+export FACTER_env_name=prod
+
+bundle exec puppet apply --modulepath="$(pwd)/modules" --hiera_config=./hiera.yaml ./casc.pp
